596de197c688cbcf42b931ffb1f1b24204cfc72d
lawaljubril <lawalj75@gmail.com>
2022-10-18 19:27:43 -0700

    Learn your times table

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *  * jack_bauer - prints every minute of the day
+ *   * Description: prints every minute of the day
+ *    * Return: void
+*/
+void jack_bauer(void)
+{
+	int hours_tens, hours_ones, mi		while (minutes_ones < 58)
+																																										{
