f5d24872006f504be392167062cc6a8c8178e6e8
lawaljubril <lawalj75@gmail.com>
2022-10-31 19:07:43 -0700

    Chess is mental torture

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_diagsums - prints sum of #'s in diagnols of square
@@ -10,12 +9,17 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i,
-	int j,
-	sum1 = 0,
-	sum2 = 0;
+	int i, j, p, l = 0, r = 0;
 
-	for (j = 0, i = size - 1; j < (size * size); j += size + 1, i += size - 1)
-	sum1 += a[j], sum2 += a[i];
-	printf("%d, %d\n", sum1, sum2);
+	for (i = 0; i < size; i++)
+	{
+		p = (i * size) + i;
+		l += *(a + p);
+	}
+	for (j = 0; j < size; j++)
+	{
+	p = (j * size) + (size - 1 - j);
+	r += *(a + p);
+	}
+	printf("%i, %i\n", l, r);
 }
