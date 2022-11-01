66005c84e7228726a2f2efccff9239d990a7257f
lawaljubril <lawalj75@gmail.com>
2022-10-31 18:51:30 -0700

    Chess is mental torture

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,12 +10,12 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, 
-    	int j,
+	int i,
+	int j,
 	sum1 = 0,
 	sum2 = 0;
 
 	for (j = 0, i = size - 1; j < (size * size); j += size + 1, i += size - 1)
-			sum1 += a[j], sum2 += a[i];
+	sum1 += a[j], sum2 += a[i];
 	printf("%d, %d\n", sum1, sum2);
 }
